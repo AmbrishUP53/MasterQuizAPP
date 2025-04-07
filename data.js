1b6741e6a8f69ad23fab95cff1d348fc1ab79f3d
AmbrishUP53 <ambrish12495@gmail.com>
2025-04-07 19:36:55 +0530

    adding data

--- data.js
+++ data.js
@@ -1,4 +1,10 @@
-const data = {
-  question : "what is the ph value of water ?",
-  answer : "5.4"
-}
+const data = [
+  {
+    question : "what is the ph value of water ?",
+    answer : "5.4"
+  },
+  {
+    question : "what is the formula of water ?",
+    answer : "H2O"
+  },
+]
